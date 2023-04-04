71739b4746f727634aff15a6e0890956381abdf9
Sameer <shaikhsksameer2511@gmail.com>
2023-04-04 21:32:25 +0530

    ner model is wrapped in flask microservice and a file flask_app.py is added

---
+++ src/flask_app.py
@@ -0,0 +1,30 @@
+import spacy
+
+nlp = spacy.load("../model/model-best")
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # extract input data from request
+    data = request.get_json()
+
+    # extract text from input data
+    text = data['text']
+
+    # use spaCy NER model to generate predictions
+    doc = nlp(text)
+
+    # extract named entities from doc
+    entities = [{'text': ent.text, 'label': ent.label_} for ent in doc.ents]
+
+    # return predictions as JSON
+    return jsonify(entities)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
